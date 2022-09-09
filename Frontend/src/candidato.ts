5d35e742620446af22bc938b91618651a87aca5e
João Gabriel Marques Mendes <102752480+jgmarquesm@users.noreply.github.com>
2022-09-09 10:36:19 -0300

    Update candidato.ts

--- Frontend/src/candidato.ts
+++ Frontend/src/candidato.ts
@@ -1,3 +1,24 @@
 export class Candidato {
     
+     constructor(public nomeCandidato: string, public cpf: string, public telefone: string,
+        public linkedin: string, public portifolio: string, public skill: string, public level: string,
+        public xp: string, public resumo: string){}
+
+    toString(): string {
+        return `"nomeCandidato": ${this.nomeEmpresa},
+        "cpf": ${this.cpf},
+        "fone": ${this.telefone},
+        "linkedin": ${this.linkedin},
+        "portifolio": ${this.portifolio},
+        "skill": ${this.skill},
+        "level": ${this.level},
+        "xp": ${this.xp},
+        "resumo": ${this.resumo}
+        `
+    }
+
+    retornoAPI(): JSON {
+        return JSON.parse(this.toString());
+    }
+    
 }
