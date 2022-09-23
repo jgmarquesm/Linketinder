65e99f312817a649fc3e29acd609bfb95fd192a5
João Gabriel Marques Mendes <102752480+jgmarquesm@users.noreply.github.com>
2022-09-23 16:05:52 -0300

    Update plotarGrafico.ts

--- Frontend/src/plotarGrafico.ts
+++ Frontend/src/plotarGrafico.ts
@@ -5,18 +5,17 @@ import { candidatos } from './main';
  export default function extrairDados(){
 
     var listaDeSkillsUnicas: Array<string> = new Array();
-	var qtdCandidatosComCadaSkill: Array<string> = new Array();
+    var qtdCandidatosComCadaSkill: Array<string> = new Array();
     
-	for (let cand of candidatos){
-	    for (let hab of cand.habilidades()){
-		qtdCandidatosComCadaSkill[listaDeSkillsUnicas.indexOf(hab)] = 0;
-
-		if (!listaDeSkillsUnicas.includes(hab)){
-	   	listaDeSkillsUnicas.push(hab);
-		}
+    for (let cand of candidatos){
+	for (let hab of cand.habilidades()){
+	    qtdCandidatosComCadaSkill[listaDeSkillsUnicas.indexOf(hab)] = 0;
+	    if (!listaDeSkillsUnicas.includes(hab)){
+  	        listaDeSkillsUnicas.push(hab);
 	    }
 	}
-    console.log(listaDeSkillsUnicas);
+    }
+    
     for (let cand of candidatos){
   	for (let hab of listaDeSkillsUnicas){
     	    if(cand.habilidades().includes(hab)){
@@ -25,7 +24,11 @@ import { candidatos } from './main';
   	}
     }
 
+    var x = listaDeSkillsUnicas;
     var y = qtdCandidatosComCadaSkill.slice(0, )
+    
+    localStorage.setItem("x", x.toString());
+    localStorage.setItem("y", y.toString());
 
         // essa função irá extrair o dados para a plotagem do gráfico e os salvará no localStorage 
         // de modo que aplicação consiga consumir esses dados usando uma tag script no body do HTML
